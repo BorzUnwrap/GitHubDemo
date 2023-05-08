6593c7a407b498d3a5e94711372f9d99a4d03ec9
Zelam <killa.15@mail.ru>
2023-05-08 04:43:55 +0300

    New Commit 3

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     var anzor = "Anzor Commit"
     var second = "SecondCommit"
     var commitVar = "CommitVar"
+    
+    var anzor = "Anzor Commit"
+    var second = "SecondCommit"
+    var commitVar = "CommitVar"
+    
+    var sadasd = 1231231
+    var asdasd = "w32323"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
